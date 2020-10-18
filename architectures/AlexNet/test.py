0b716636ad842aeb3253e06cd0acb2748208416d
the-robot <9334746+the-robot@users.noreply.github.com>
2020-10-18 16:36:22 +0000

    added AlexNet architecture

--- architectures/AlexNet/test.py
+++ architectures/AlexNet/test.py
@@ -2,3 +2,8 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+from alexnet import AlexNet
+
+if __name__ == "__main__":
+    model = AlexNet(input_shape = (224, 224, 3), classes = 1000)
+    model.summary()
