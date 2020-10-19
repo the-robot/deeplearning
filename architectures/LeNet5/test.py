c31c1b6aae38016fee4a2f80ac25257b64d6ef21
the-robot <9334746+the-robot@users.noreply.github.com>
2020-10-19 16:49:10 +0000

    added LeNet5 architecture

--- architectures/LeNet5/test.py
+++ architectures/LeNet5/test.py
@@ -2,3 +2,8 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+from lenet5 import LeNet5
+
+if __name__ == "__main__":
+    model = LeNet5(input_shape = (32, 32, 1), classes = 10)
+    model.summary()
